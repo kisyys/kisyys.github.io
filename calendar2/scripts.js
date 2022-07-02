a27f3ed4e1f7bd391ca530bacabe8b8f48c3e566
kisyys <93004954+kisyys@users.noreply.github.com>
2022-07-02 14:57:20 +0300

    Update scripts.js

--- calendar2/scripts.js
+++ calendar2/scripts.js
@@ -265,8 +265,9 @@ function handleTouchMove(evt) {
                                                                          
     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
         if ( xDiff > 0 ) {
-            back(0);
+            step(-1);
         } else {
+            step(1);
             /* left swipe */
         }                       
     } else {
