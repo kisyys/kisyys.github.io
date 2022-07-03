50c85b354238669d5d996ed8858f76906d1309ba
kisyys <93004954+kisyys@users.noreply.github.com>
2022-07-03 19:59:58 +0300

    Update scripts.js

--- calendar2/scripts.js
+++ calendar2/scripts.js
@@ -293,57 +293,57 @@ function whatMonth(number) {
     }   
 }
 
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
+// document.addEventListener('touchstart', handleTouchStart, false);        
+// document.addEventListener('touchmove', handleTouchMove, false);
 
 
-var xDown = null;                                                        
-var yDown = null;
+// var xDown = null;                                                        
+// var yDown = null;
 
-function getTouches(evt) {
-  return evt.touches ||             // browser API
-         evt.originalEvent.touches; // jQuery
-}                                                     
+// function getTouches(evt) {
+//   return evt.touches ||             // browser API
+//          evt.originalEvent.touches; // jQuery
+// }                                                     
                                                                          
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+// function handleTouchStart(evt) {
+//     const firstTouch = getTouches(evt)[0];                                      
+//     xDown = firstTouch.clientX;                                      
+//     yDown = firstTouch.clientY;                                      
+// };                                                
                                                                          
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+// function handleTouchMove(evt) {
+//     if ( ! xDown || ! yDown ) {
+//         return;
+//     }
 
-    var xUp = evt.touches[0].clientX;                                    
-    var yUp = evt.touches[0].clientY;
+//     var xUp = evt.touches[0].clientX;                                    
+//     var yUp = evt.touches[0].clientY;
 
-    var xDiff = xDown - xUp;
-    var yDiff = yDown - yUp;
+//     var xDiff = xDown - xUp;
+//     var yDiff = yDown - yUp;
                                                                          
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-        if ( xDiff > 0 ) {
-            /* left swipe */
-            if(display == 0) {
-                step(1);
-            }
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+//         if ( xDiff > 0 ) {
+//             /* left swipe */
+//             if(display == 0) {
+//                 step(1);
+//             }
             
-        } else {
-            /* right swipe */
-            if(display == 0) {
-                step(-1);
-            }
+//         } else {
+//             /* right swipe */
+//             if(display == 0) {
+//                 step(-1);
+//             }
             
-        }                       
-    } else {
-        if ( yDiff > 0 ) {
-            /* down swipe */ 
-        } else { 
-            /* up swipe */
-        }                                                                 
-    }
-    /* reset values */
-    xDown = null;
-    yDown = null;                                             
-};
+//         }                       
+//     } else {
+//         if ( yDiff > 0 ) {
+//             /* down swipe */ 
+//         } else { 
+//             /* up swipe */
+//         }                                                                 
+//     }
+//     /* reset values */
+//     xDown = null;
+//     yDown = null;                                             
+// };
