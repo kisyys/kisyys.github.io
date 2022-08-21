adc7c7a00c081c9eb601228c3262502a9ace4070
kisyys <93004954+kisyys@users.noreply.github.com>
2022-08-21 21:33:48 +0300

    Create App.js

---
+++ fs2022/osa1/unicafe/src/App.js
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good+1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral+1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad+1)} text="Bad" />
+      <h1> Statistics </h1>
+      Good {good}
+      <br></br>
+      Neutral {neutral}
+      <br></br>
+      Bad {bad}
+    </div>
+  )
+}
+
+export default App
