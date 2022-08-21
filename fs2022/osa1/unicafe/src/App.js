d795a6a20b8692e23655e677a389eea86dfa3869
kisyys <93004954+kisyys@users.noreply.github.com>
2022-08-21 22:00:06 +0300

    Update App.js

--- fs2022/osa1/unicafe/src/App.js
+++ fs2022/osa1/unicafe/src/App.js
@@ -6,6 +6,25 @@ const Button = (props) => (
   </button>
 )
 
+const Statistics = (props) => {
+  return (
+    <div>
+      <h1> Statistics </h1>
+      Good {props.good}
+      <br></br>
+      Neutral {props.neutral}
+      <br></br>
+      Bad {props.bad}
+      <br></br>
+      All {props.good + props.neutral + props.bad}
+      <br></br>
+      Average {(props.good*1 + props.neutral*0 + props.bad*(-1))/(props.good + props.neutral + props.bad)}
+      <br></br>
+      Positive {props.good/(props.good + props.neutral + props.bad)*100} %
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -18,18 +37,7 @@ const App = () => {
       <Button handleClick={() => setGood(good+1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral+1)} text="Neutral" />
       <Button handleClick={() => setBad(bad+1)} text="Bad" />
-      <h1> Statistics </h1>
-      Good {good}
-      <br></br>
-      Neutral {neutral}
-      <br></br>
-      Bad {bad}
-      <br></br>
-      All {good + neutral + bad}
-      <br></br>
-      Average {(good*1 + neutral*0 + bad*(-1))/(good + neutral + bad)}
-      <br></br>
-      Positive {good/(good + neutral + bad)*100} %
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
