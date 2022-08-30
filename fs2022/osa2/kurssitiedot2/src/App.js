7fb62837fd969cccba65d36eb23c5542c8906719
kisyys <93004954+kisyys@users.noreply.github.com>
2022-08-30 23:03:24 +0300

    Update App.js

--- fs2022/osa2/kurssitiedot2/src/App.js
+++ fs2022/osa2/kurssitiedot2/src/App.js
@@ -1,22 +1,4 @@
-const Course = (props) => {
-  return (
-    <div>
-      {props.courses.map(c1 => (
-        <div key={c1.id}>  
-            <h2>
-              {c1.name}
-            </h2>           
-          {c1.parts.map(p1 =>
-            <p key={p1.id}>         
-                {p1.name} {p1.exercises} 
-            </p>
-          )}
-          <b> total of {c1.parts.reduce((sum, c2) => sum = sum + c2.exercises, 0)} exercises </b>
-        </div>
-      ))}
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
